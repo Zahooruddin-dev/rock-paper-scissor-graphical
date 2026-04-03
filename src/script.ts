091bbd7dfe350ecc18544a00bff263ed464642e7
zahooruddin-dev <mzkhan886@gmail.com>
2026-04-03 14:40:14 +0500

    rock-paper-scissor-typescript

--- src/script.ts
+++ src/script.ts
@@ -1,66 +1,64 @@
-import {matchAnimation} from"./scripts/matchLogic.js"
+import { matchAnimation } from './scripts/matchLogic.js';
 
-const chooses = document.querySelector("#chooses") as HTMLDivElement;
-const audio = document.querySelector("audio") as HTMLAudioElement;
+const chooses = document.querySelector('#chooses') as HTMLDivElement;
+const audio = document.querySelector('audio') as HTMLAudioElement;
 audio.volume = 0.8;
 
-let chooseOfPlayer : string;
-let chooseOfMachine : string;
+let chooseOfPlayer: string ; 
+let chooseOfMachine: number | string;
 
-let heartsPlayer : number = 5;
-let heartsComputer : number = 5;
-const audiobg = document.getElementById("bgm") as HTMLAudioElement;
+let heartsPlayer: number = 5;
+let heartsComputer: number = 5;
+const audiobg = document.getElementById('bgm') as HTMLAudioElement;
 audiobg.volume = 0.2;
 
-document.addEventListener("click", () => {
-  audiobg.play();
-}, { once: true });
+document.addEventListener(
+	'click',
+	() => {
+		audiobg.play();
+	},
+	{ once: true },
+);
 
+chooses.addEventListener('click', async (e) => {
+	const audioClicked = new Audio('./music/selectedButton.mp3');
+	audioClicked.volume = 1;
+	audioClicked.play();
+	chooseOfPlayer = e.target.alt as string;
+	chooseOfMachine = Math.floor(Math.random() * 3) + 1;
 
+	switch (chooseOfMachine) {
+		case 1:
+			chooseOfMachine = 'rock';
+			break;
+		case 2:
+			chooseOfMachine = 'paper';
+			break;
+		case 3:
+			chooseOfMachine = 'scissor';
+			break;
+		default:
+			console.log('Choose a valid value');
+			break;
+	}
+	matchAnimation(chooseOfPlayer, chooseOfMachine);
+});
 
-chooses.addEventListener("click", async e => {
-    const audioClicked = new Audio("./music/selectedButton.mp3");
-    audioClicked.volume = 1;
-    audioClicked.play();
-    console.log(e.target.alt);
+chooses.addEventListener('mouseover', (e) => {
+	const audioHover = new Audio('./music/hoverAudio.mp3');
+	audioHover.volume = 1;
 
-    chooseOfPlayer = e.target.alt as string;
-    chooseOfMachine = Math.floor(Math.random() * 3) + 1;
-
-    switch (chooseOfMachine){
-        case 1:
-            chooseOfMachine = "rock";
-            break;
-        case 2:
-            chooseOfMachine = "paper";
-            break;
-        case 3:
-            chooseOfMachine = "scissor";
-            break;
-        default:
-            console.log("Choose a valid value");
-            break;
-    }
-    matchAnimation(chooseOfPlayer, chooseOfMachine)
-})
-
-chooses.addEventListener("mouseover", e => {
-    const audioHover = new Audio("./music/hoverAudio.mp3");
-    audioHover.volume = 1;
-
-    switch(e.target.alt as string){
-        case "rock":
-            audioHover.play();
-            break;
-        case "paper":
-            audioHover.play();
-            break;
-        case "scissor":
-            audioHover.play();
-            break;
-        default:
-            break;
-    }
-        
-    })
-
+	switch (e.target.alt as string) {
+		case 'rock':
+			audioHover.play();
+			break;
+		case 'paper':
+			audioHover.play();
+			break;
+		case 'scissor':
+			audioHover.play();
+			break;
+		default:
+			break;
+	}
+});
